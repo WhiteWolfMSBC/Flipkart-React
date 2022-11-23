6fd63c6a8abee909248245f6cd44cd5cb2e8eaa3
Sachin Sourabh <Sachin.Sourabh@MSBC-MAINFRAME.LCL>
2022-11-23 16:19:31 +0530

    Updates

---
+++ precache-manifest.ed8e47d6ad72ec6b810f6b5e7bf588cb.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b925ab2749b8659f9ea",
+    "url": "/Flipkart-React/static/css/main.8ac1cf02.chunk.css"
+  },
+  {
+    "revision": "8b925ab2749b8659f9ea",
+    "url": "/Flipkart-React/static/js/main.00f3652b.chunk.js"
+  },
+  {
+    "revision": "153ec4c6f0bee3b6d9bd",
+    "url": "/Flipkart-React/static/js/runtime~main.4f059d54.js"
+  },
+  {
+    "revision": "ff0c6ab1af7b13ae4b94",
+    "url": "/Flipkart-React/static/js/2.a610283c.chunk.js"
+  },
+  {
+    "revision": "b0b4ca849d9f775df821",
+    "url": "/Flipkart-React/static/js/3.8dcf77dc.chunk.js"
+  },
+  {
+    "revision": "44d3808d6ac0929eb76916fd5ac04ae0",
+    "url": "/Flipkart-React/static/media/52.44d3808d.png"
+  },
+  {
+    "revision": "39abbac4f44f3cce9bfe795943ce271c",
+    "url": "/Flipkart-React/static/media/20.39abbac4.png"
+  },
+  {
+    "revision": "ca37fbd93109df0bfc942663fcb13520",
+    "url": "/Flipkart-React/static/media/18.ca37fbd9.png"
+  },
+  {
+    "revision": "c430324f7c5b159a12828cfa83756c8f",
+    "url": "/Flipkart-React/static/media/22.c430324f.png"
+  },
+  {
+    "revision": "659b325b875b6e7580e1adcb4637024f",
+    "url": "/Flipkart-React/static/media/21.659b325b.png"
+  },
+  {
+    "revision": "ea30f9c50c81687486d8239f317face9",
+    "url": "/Flipkart-React/static/media/23.ea30f9c5.png"
+  },
+  {
+    "revision": "fb24873c0f4eeaee164c28daa712a560",
+    "url": "/Flipkart-React/static/media/24.fb24873c.png"
+  },
+  {
+    "revision": "ddcbdf76a58c469c853858d67eb64c49",
+    "url": "/Flipkart-React/static/media/25.ddcbdf76.png"
+  },
+  {
+    "revision": "3dcd34bbebf881f7b91907ccfb1616ac",
+    "url": "/Flipkart-React/static/media/26.3dcd34bb.png"
+  },
+  {
+    "revision": "a38a93857152a10abbd61782ef263519",
+    "url": "/Flipkart-React/static/media/27.a38a9385.png"
+  },
+  {
+    "revision": "4ac22a5e400751c9a8529963cefb01a6",
+    "url": "/Flipkart-React/static/media/28.4ac22a5e.png"
+  },
+  {
+    "revision": "95d6d51f48f6ad4cc35a0da0ac432aa8",
+    "url": "/Flipkart-React/static/media/29.95d6d51f.png"
+  },
+  {
+    "revision": "ddf784c5e313d342be8a2b30e103d7fe",
+    "url": "/Flipkart-React/static/media/30.ddf784c5.png"
+  },
+  {
+    "revision": "67ebd076b506a82802783f022b812983",
+    "url": "/Flipkart-React/static/media/1.67ebd076.jpg"
+  },
+  {
+    "revision": "5efddf54f6bba868056ca2aefdf4f6df",
+    "url": "/Flipkart-React/static/media/3.5efddf54.png"
+  },
+  {
+    "revision": "70673495ac88b358837f833b5d79017f",
+    "url": "/Flipkart-React/static/media/2.70673495.png"
+  },
+  {
+    "revision": "3200642c0c4efe04538d49532198efa4",
+    "url": "/Flipkart-React/static/media/4.3200642c.png"
+  },
+  {
+    "revision": "419cc2c8cbca2429d5a0ac9275883a13",
+    "url": "/Flipkart-React/static/media/6.419cc2c8.png"
+  },
+  {
+    "revision": "f8c7acd4b5aeea3632c1c0f47713c135",
+    "url": "/Flipkart-React/static/media/5.f8c7acd4.png"
+  },
+  {
+    "revision": "d6cdaa5d771ee0b6001b9a416a2cfb00",
+    "url": "/Flipkart-React/static/media/10.d6cdaa5d.png"
+  },
+  {
+    "revision": "433a789474704e3f8043e9ba5ce49782",
+    "url": "/Flipkart-React/static/media/8.433a7894.png"
+  },
+  {
+    "revision": "6f7cda37a90b578e3def395ca8ede70b",
+    "url": "/Flipkart-React/static/media/7.6f7cda37.png"
+  },
+  {
+    "revision": "3e399b7a6cdcb74c0d953116441c888b",
+    "url": "/Flipkart-React/static/media/31.3e399b7a.jpg"
+  },
+  {
+    "revision": "4c28801e4fa3de5b9c38478f48f25e64",
+    "url": "/Flipkart-React/static/media/32.4c28801e.jpg"
+  },
+  {
+    "revision": "b08443efea056163acde8370178618d1",
+    "url": "/Flipkart-React/static/media/33.b08443ef.jpg"
+  },
+  {
+    "revision": "581d6e04338527ea5a14212f4f022cc0",
+    "url": "/Flipkart-React/static/media/40.581d6e04.jpg"
+  },
+  {
+    "revision": "21688cd3e1074268fc03725395ecb435",
+    "url": "/Flipkart-React/static/media/41.21688cd3.jpeg"
+  },
+  {
+    "revision": "1e78ed15d526f90e5d924e93816ff22f",
+    "url": "/Flipkart-React/static/media/42.1e78ed15.jpeg"
+  },
+  {
+    "revision": "00a4aee2229b6e72fb91aa96b2e6d918",
+    "url": "/Flipkart-React/static/media/34.00a4aee2.jpg"
+  },
+  {
+    "revision": "df0befba4de4c0542a7e3bad3cee8541",
+    "url": "/Flipkart-React/static/media/35.df0befba.jpg"
+  },
+  {
+    "revision": "b4cff572c8450059015db613a1ba17cc",
+    "url": "/Flipkart-React/static/media/36.b4cff572.jpg"
+  },
+  {
+    "revision": "20687f261f4434a6296cfd8a8949fd62",
+    "url": "/Flipkart-React/static/media/37.20687f26.png"
+  },
+  {
+    "revision": "2fc9fd8b236546d33c5458ec2a9c41f6",
+    "url": "/Flipkart-React/static/media/38.2fc9fd8b.png"
+  },
+  {
+    "revision": "2107d9476b4baffe793a54c3dc7b4fcd",
+    "url": "/Flipkart-React/static/media/39.2107d947.png"
+  },
+  {
+    "revision": "6e12828a8fa4d96357a4c329b7f8362d",
+    "url": "/Flipkart-React/static/media/43.6e12828a.png"
+  },
+  {
+    "revision": "22cfee90e38129e59ca80abdf8295213",
+    "url": "/Flipkart-React/static/media/44.22cfee90.png"
+  },
+  {
+    "revision": "70480b7278f482e689f608ac086f1b9e",
+    "url": "/Flipkart-React/static/media/45.70480b72.png"
+  },
+  {
+    "revision": "8c7ca38a72fe77701c60a68af0dfc86a",
+    "url": "/Flipkart-React/static/media/47.8c7ca38a.png"
+  },
+  {
+    "revision": "4687f84aeeaf64a20d45c4cd0ac884da",
+    "url": "/Flipkart-React/static/media/49.4687f84a.png"
+  },
+  {
+    "revision": "6fb372ebcd2c04ff2b989db04c947ff8",
+    "url": "/Flipkart-React/static/media/50.6fb372eb.png"
+  },
+  {
+    "revision": "5a586398ec70a742bceeb981be075c85",
+    "url": "/Flipkart-React/static/media/51.5a586398.png"
+  },
+  {
+    "revision": "735c4d10111ab1563ca96738ff767264",
+    "url": "/Flipkart-React/static/media/12.735c4d10.png"
+  },
+  {
+    "revision": "71383f35687dc98d60d925ca12ac67a2",
+    "url": "/Flipkart-React/static/media/13.71383f35.jpg"
+  },
+  {
+    "revision": "ed0be84d3c388abf9495ba3bb730b0f9",
+    "url": "/Flipkart-React/static/media/14.ed0be84d.png"
+  },
+  {
+    "revision": "71c1a7fce175301f1b241011c343d227",
+    "url": "/Flipkart-React/static/media/15.71c1a7fc.jpg"
+  },
+  {
+    "revision": "4117689b58d01fd6af0097c645840e3a",
+    "url": "/Flipkart-React/static/media/16.4117689b.jpg"
+  },
+  {
+    "revision": "efc5ebfd574b282059897d6a92c16152",
+    "url": "/Flipkart-React/static/media/17.efc5ebfd.jpg"
+  },
+  {
+    "revision": "1e84ba67f19ab7ebb2f28412e2b9e9d0",
+    "url": "/Flipkart-React/static/media/53.1e84ba67.png"
+  },
+  {
+    "revision": "ceaeb8654f2e5e4f4456518b01b2338a",
+    "url": "/Flipkart-React/static/media/54.ceaeb865.png"
+  },
+  {
+    "revision": "5d65bf31091105fd43d7932f8b4aae65",
+    "url": "/Flipkart-React/index.html"
+  }
+];
